89b8fdea19d6d340553ba4528edea3b51513b8c8
eugene-zheltov <eugene.zheltov@exactprosystems.com>
2021-09-22 18:39:36 +0400

    Change Converter behavior

--- src/main/kotlin/com/exactpro/th2/converter/Converter.kt
+++ src/main/kotlin/com/exactpro/th2/converter/Converter.kt
@@ -1,7 +1,9 @@
 package com.exactpro.th2.converter
 
+import com.exactpro.th2.codec.xml.XmlPipelineCodecFactory
 import com.exactpro.th2.common.grpc.ListValue
 import com.exactpro.th2.common.grpc.Message
+import com.exactpro.th2.common.grpc.RawMessage
 import com.exactpro.th2.common.grpc.Value
 import com.exactpro.th2.common.message.*
 import com.exactpro.th2.common.value.getMessage
@@ -24,6 +26,7 @@ class Converter {
                 is ObjectNode -> {
                     return Value.newBuilder().apply {
                         messageValueBuilder.apply {
+                            messageType = name
                             node.fieldNames().forEach {
                                 putFields(it, jsonToValue(node[it], it))
                             }
@@ -33,17 +36,28 @@ class Converter {
                 is ArrayNode -> {
                     return Value.newBuilder().apply {
                         messageValueBuilder.apply {
+                            messageType = name
                             listValue = listValueBuilder.addAllValues(node.toListValue().valuesList).build()
 
                             val listValueBuilder = ListValue.newBuilder()
 
-                            node.toMutableList().forEach { listValue = listValueBuilder.addValues(Value.newBuilder().setSimpleValue(it.textValue())).build() }
+                            node.toMutableList().forEach {
+                                val textValue = it.textValue()
+
+                                if (textValue != null) {
+                                    listValue =
+                                        listValueBuilder.
+                                        addValues(Value.newBuilder()
+                                            .setSimpleValue(textValue)).build()
+                                }
+                            }
                         }
                     }.build()
                 }
                 is ValueNode -> {
                     return Value.newBuilder().apply {
                         messageValueBuilder.apply {
+                            messageType = name
                             this[name] = node.textValue()
                         }
                     }.build()
@@ -52,7 +66,18 @@ class Converter {
             }
         }
 
-        fun convertJsonToProto(node: JsonNode) : Message = jsonToValue(node).getMessage()
+        fun convertJsonToProto(node: JsonNode, type: String, rawMessage: RawMessage) : Message = jsonToValue(node, type).getMessage()!!
+            .toBuilder().apply {
+                parentEventId = rawMessage.parentEventId
+                metadataBuilder.also { msgMetadata ->
+                    rawMessage.metadata.also { rawMetadata ->
+                        msgMetadata.id = rawMetadata.id
+                        msgMetadata.timestamp = rawMetadata.timestamp
+                        msgMetadata.protocol = XmlPipelineCodecFactory.PROTOCOL
+                        msgMetadata.putAllProperties(rawMetadata.propertiesMap)
+                    }
+                }
+            }.build()
             ?: throw IllegalArgumentException("JsonNode $node does not contain a message")
 
         fun convertProtoToJson(message: Message) : String = ObjectMapper().createObjectNode().apply {
